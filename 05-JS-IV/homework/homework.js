ab9943fc288fe78521511ded099fa650adc05df0
omar1779 <josemyclans@gmail.com>
2022-04-27 00:31:34 -0500

    Homeworks Completados

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -126,6 +126,10 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  for(i = 0; i < usuarios.length; i ++ ){
+    usuarios[i].esPremium = true;
+  }
+  return usuarios;
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -135,6 +139,11 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  let sumaDeLikes = 0;
+  for(i=0; i<usuario.posts.length; i++){
+    sumaDeLikes = sumaDeLikes + usuario.posts[i].likes;
+  }
+  return sumaDeLikes;
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -147,7 +156,11 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto.calcularPrecioDescuento = function (){
+    let descuento = producto.precio - (producto.precio * producto.porcentajeDeDescuento)
+    return descuento;
+  }
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
