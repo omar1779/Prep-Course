818fc5709f8846db7913f582db00aeabe619877e
omar1779 <josemyclans@gmail.com>
2022-05-04 23:21:58 -0500

    homeworks 50%

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -4,16 +4,30 @@ function crearUsuario() {
   // Crea una Clase de ES6 o una función constructor llamada "Usuario"
   // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
   // En el `contructor`, define el usuario, el nombre, el email y la contraseña
-  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
+  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 
+  //'Hola, mi nombre es {{nombre}}'
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  function Usuario (opciones){
+    this.usuario = opciones.usuario,
+    this.nombre = opciones.nombre,
+    this.email = opciones.email,
+    this.password = opciones.password
+  }
+  Usuario.prototype.saludar = function(){
+    return 'Hola, mi nombre es ' + this.nombre;
+  }
+  return Usuario;
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function () {
+    return 'Hello World!'
+  }
 }
 
 function agregarStringInvertida() {
@@ -22,6 +36,9 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+  String.prototype.reverse = function () {
+    return Array.from(this).reverse().join("");
+  }
 }
 
 // ---------------------------------------------------------------------------//
